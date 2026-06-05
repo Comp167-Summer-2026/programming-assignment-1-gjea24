5163c8f094a0ce1045d4f597bd4b8fe97638e6a5
gavjb <gjeanbaptiste@aggies.ncat.edu>
2026-06-05 14:23:38 -0400

    third push

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -4,7 +4,7 @@ public class TemperatureConverter {
 
     public static double convertTemperature(double temperature, String unit) {
 
-        if (unit.equals("C")) {
+        if (unit.equalsIgnoreCase("C")) {
             return (temperature * 9.0 / 5.0) + 32;
         }
         else {
@@ -28,32 +28,35 @@ public class TemperatureConverter {
                 double temperature = scnr.nextDouble();
                 scnr.nextLine();
 
-                System.out.print("Enter the unit (C or F): ");
-                String unit = scnr.nextLine();
+                String unit = "";
 
-                if (unit.equals("C") || unit.equals("F")) {
+                while (!(unit.equalsIgnoreCase("C") || unit.equalsIgnoreCase("F"))) {
 
-                    double convertedTemp =
-                            convertTemperature(temperature, unit);
+                    System.out.print("Enter the unit (C or F): ");
+                    unit = scnr.nextLine();
 
-                    if (unit.equals("C")) {
-                        System.out.printf("%.2f C is equal to %.2f F%n",
-                                temperature, convertedTemp);
-                    }
-                    else {
-                        System.out.printf("%.2f F is equal to %.2f C%n",
-                                temperature, convertedTemp);
+                    if (!(unit.equalsIgnoreCase("C") || unit.equalsIgnoreCase("F"))) {
+                        System.out.println("Error: Invalid unit.");
                     }
                 }
+
+                double convertedTemp =
+                        convertTemperature(temperature, unit);
+
+                if (unit.equalsIgnoreCase("C")) {
+                    System.out.printf("%.2f C is equal to %.2f F%n",
+                            temperature, convertedTemp);
+                }
                 else {
-                    System.out.println("Error: Invalid unit.");
+                    System.out.printf("%.2f F is equal to %.2f C%n",
+                            temperature, convertedTemp);
                 }
             }
             else {
 
                 userInput = scnr.nextLine();
 
-                if (userInput.equals("stop")) {
+                if (userInput.equalsIgnoreCase("stop")) {
                     keepGoing = false;
                 }
                 else {
